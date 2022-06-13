7e3b12abd8e0a7db7de51542cd8dedf6b15af4a4
mktcode <kontakt@markus-kottlaender.de>
2022-06-13 22:55:23 +0200

    updated deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,7 +3,7 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -14,12 +14,13 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const BoostManager = await ethers.getContractFactory("BoostManager");
+  const boostManager = await BoostManager.deploy();
 
-  await greeter.deployed();
+  await boostManager.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("BoostManager deployed to:", boostManager.address);
+  console.log("One network:", network.name);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
